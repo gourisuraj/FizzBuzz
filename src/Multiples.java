cda082106b04b5a7934f368b836cee087fbb1b5d
gourisuraj <gouri.suraj@mail.utoronto.ca>
2024-05-30 15:59:16 -0400

    "multiples added"

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,16 +1,16 @@
 public class Multiples {
     public static void main(String[] args) {
         int i = 1000;
-        int count_mulitples = 0;
+        int multiple_count = 0;
         for (int o = 1; o < i; o++) {
 
             boolean divisibleBy3 = o % 3 == 0;
             boolean divisibleBy5 = o % 5 == 0;
 
             if (divisibleBy3 || divisibleBy5) {
-                count_mulitples += 1;
+                multiple_count += 1;
             }
         }
-        System.out.println(count_mulitples);
+        System.out.println(multiple_count);
     }
 }
